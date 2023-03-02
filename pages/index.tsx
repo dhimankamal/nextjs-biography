c9aba2a031db5ff971e4c5876dd9734cc3cdbc47
kamal <dhimank079@gmail.com>
2023-03-02 21:03:02 +0530

    add husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ const Home: NextPage<Props> = ({ post }) => {
               <div key={val.id} className="border-b dark:border-gray-800">
                 <div className="py-8 flex flex-wrap md:flex-nowrap">
                   <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                    <span className="font-semibold title-font text-gray-700 dark:text-gray-300">
+                        <span className="font-semibold title-font text-gray-700 dark:text-gray-300">
                       {categoryName?.name || "CATEGORY"}
                     </span>
                     <span className="mt-1 text-gray-500  text-sm">
