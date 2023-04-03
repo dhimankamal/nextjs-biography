86822bea1cf91d6a84dac62b7dfe167d53373b58
kamal <dhimank079@gmail.com>
2023-04-03 18:11:00 +0530

    update ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <script
+        <script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2446438232158113"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4639278973897914"
           crossOrigin="anonymous"
-        ></script> */}
+        ></script>
       </Head>
       <body>
         <Main />
