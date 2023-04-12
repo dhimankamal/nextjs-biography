4e669a4704615f9bfe13ba525b456d687f305cd1
kamal <dhimank079@gmail.com>
2023-04-12 21:07:17 +0530

    add script

--- components/ads/HorizontalAd.tsx
+++ components/ads/HorizontalAd.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { useEffect } from "react";
 
 declare global {
@@ -14,14 +15,21 @@ const HorizontalAd = () => {
   }, []);
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "block" }}
-      data-ad-client="ca-pub-2446438232158113"
-      data-ad-slot="4110269193"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    ></ins>
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2446438232158113"
+        crossOrigin="anonymous"
+      ></Script>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-2446438232158113"
+        data-ad-slot="4110269193"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </>
   );
 };
 
