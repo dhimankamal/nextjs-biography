f4825ecf198a0a857ca37e59df3025bc4922e247
kamal <dhimank079@gmail.com>
2023-05-16 07:18:10 +0530

    update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,9 @@
 import { NextPage } from "next";
-import { Post } from "@prisma/client";
-import { useState } from "react";
-import axios from "axios";
 
-interface Props {
-  post: Post[];
-}
+interface Props {}
 
-const Home: NextPage<Props> = ({ post }) => {
-  const [page, setPage] = useState(2);
-  const [items, setItems] = useState(post);
-  const [hasMore, setHasmore] = useState(true);
-
-  const handleLoadMore = async () => {
-    setPage(page + 1);
-    const res = await axios({
-      method: "GET",
-      url: `/api/post/getpost?page=${page}`,
-    });
-    if (res.data && res.data.length > 0) {
-      setItems([...items, ...res.data]);
-    } else {
-      setHasmore(false);
-    }
-  };
-
+const Home: NextPage<Props> = ({}) => {
   return <>api</>;
 };
+
+export default Home;
